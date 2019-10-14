0827188093a3d4040356e75d76e335210f64a014
SHIMIZU Mai <21716049@jwu.ac.jp>
2019-10-14 16:38:11 +0900

    add4

--- exp.py
+++ exp.py
@@ -21,6 +21,10 @@ assert isinstance(v,int)
 class Add(Expr):
     __slots__=['left','right']
     def __init__(self,a,b):
+        if not isinstance(a,Expr):
+            a = Val(a)
+        if not isinstance(b,Expr):
+            a = Val(b)
         self.left = a
         self.right = b
     def eval(self):
@@ -32,6 +36,9 @@ assert e.eval() == 3
 e = Add(Val(1),Add(Val(2),Val(3)))
 print(e.eval())
 assert e.eval() == 6
+e = Add(1,Add(1,2))
+print(e.eval())
+assert e.eval() == 4
 
 
 
