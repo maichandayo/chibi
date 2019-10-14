f403bc81d022ef04f4bf42b58a14c77a2a00069c
SHIMIZU Mai <21716049@jwu.ac.jp>
2019-10-14 16:21:10 +0900

    add3

--- exp.py
+++ exp.py
@@ -1,5 +1,7 @@
 
-class Val(object):
+class Expr(object):
+    pass
+class Val(Expr):
     __slots__ = ['value']
     def __init__(self,value=0):
         self.value=value
@@ -12,7 +14,11 @@ v=Val(1)
 print(v)
 assert v.eval() == 1
 
-class Add(object):
+assert isinstance(v,Expr)
+assert isinstance(v,Val)
+assert isinstance(v,int)
+
+class Add(Expr):
     __slots__=['left','right']
     def __init__(self,a,b):
         self.left = a
