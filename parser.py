0734dd0df776307a6ca7d05bf1518a4678ee973e
SHIMIZU Mai <21716049@jwu.ac.jp>
2019-10-28 16:36:52 +0900

    output Add,Mul

--- parser.py
+++ parser.py
@@ -18,13 +18,19 @@ print(s,s1,s2)
 '''
 
 def parse(s:str):
-    pos = s.find('+')
-    if pos == -1:
-        return Val(int(s))
-    else:
+    if s.find('+') > 0:
+        pos = s.find('+')
         s1 = s[0:pos]
         s2 = s[pos+1:]
         return Add(parse(s1),parse(s2))
+    if s.find('*') > 0:
+        pos = s.find('*')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Mul(parse(s1),parse(s2))
+    return Val(int(s))
 
 e = parse("1+2+3")
+print(e.eval())
+e = parse("1+2*3")
 print(e.eval())
